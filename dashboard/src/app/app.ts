0e77742a92d7fb6058ae455730e97f7db658d27e
Jeremy Mowery <jermowery@gmail.com>
2025-08-24 19:07:34 -0700

    Add background image

--- dashboard/src/app/app.ts
+++ dashboard/src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, resource, signal } from '@angular/core';
+import { createApi } from 'unsplash-js';
+
+const unsplashApi = createApi({
+  accessKey: '6U2sy2FY2rvAYb4L3jjaEG6xzf88PEO_h4myO0Zpsfo',
+});
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('dashboard');
+  private readonly randomUnsplashImage = resource({
+    loader: async () =>
+      await unsplashApi.photos.getRandom({
+        query: 'cute animals',
+        count: 1,
+        contentFilter: 'high',
+        orientation: 'landscape',
+      }),
+  });
+  protected readonly backgroundImageUrl = computed<string | null>(() => {
+    if (!this.randomUnsplashImage.hasValue()) {
+      console.error('no value');
+      return null;
+    }
+    const response = this.randomUnsplashImage.value().response;
+    const image = Array.isArray(response) ? response[0] : response;
+    if (!image) {
+      console.error('no image');
+      return null;
+    }
+    return image.urls.full;
+  });
 }
